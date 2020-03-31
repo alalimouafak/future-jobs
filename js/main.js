7aaa6bf9e045164ff2d6acc97a2915b5194e26c5
alalimouafak <55447538+alalimouafak@users.noreply.github.com>
2020-03-31 19:28:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-fetch("POST /connexion/oauth2/access_token?realm=%2Fpartenaire" , {
-    method: 'POST',
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded;",
-      "Host": "entreprise.pole-emploi.fr",
-      "Accept": "*/*",
-    "Content-Length": "326"
-    },
-    body: "grant_type=client_credentials&client_id=[PAR_futurejob_136378497779aeb3f34e8ff8ed5d74105c30b4e095f3f413d51b3fe808148bd7]&client_secret=[493cc9870379dbf1cc17d968876623d4646d98a2a926749c92ebb1deddb8b969]&scope=application_[PAR_futurejob_136378497779aeb3f34e8ff8ed5d74105c30b4e095f3f413d51b3fe808148bd7]%20api_offresdemploiv2 o2dsoffre"
-  })
-  .then(res=>res)
-  .then(function (data) {
-    console.log('Request succeeded with JSON response', data);
-  })
-  .catch(function (error) {
-    console.log('Request failed', error);
-  });
+var myHeaders = new Headers();
+myHeaders.append("Content-type", "application/x-www-form-urlencoded");
+myHeaders.append("Content-Type", "text/plain");
+var bodyy = "grant_type=client_credentials&client_id=PAR_futurejobs2_6195762ee13d2b607fa002bc9bed2e5b1c6b695e631f7403b6a08a8b8f3005ce&client_secret=31abcdfb6685a1b367d04b3047650e88d83863eeb43546e0185e65adf69c1860&scope=application_PAR_futurejobs2_6195762ee13d2b607fa002bc9bed2e5b1c6b695e631f7403b6a08a8b8f3005ce%20api_offresdemploiv2 o2dsoffre";
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: body,
+  redirect: 'follow'
+};
+
+fetch("https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=partenaire", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
